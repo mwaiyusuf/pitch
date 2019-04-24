578cb57758fdbda34c5953d9c6d18ff0b97396db
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-24 13:54:49 +0300

    Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_uploads import UploadSet,configure_uploads,IMAGES  #type of file to upload
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
@@ -17,7 +17,7 @@ simple = SimpleMDE()
 
 
 login_manager = LoginManager()
-login_manager.session_protection = 'strong'
+login_manager.session_protection = 'strong'  #provide diff security levls and setign it i strong 
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 
@@ -42,12 +42,12 @@ def create_app(config_name):
     simple.init_app(app)
 
 
-    # Registering the blueprint
+    # Registering the blueprint in our create app function
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')  #addin a prefix  to all the routes registered with the blueprint
 
 
     # configure UploadSet
