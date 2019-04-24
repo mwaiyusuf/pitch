c503fe00aeaa32c6b9c8704057f062c9d5cceeb3
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-24 14:09:39 +0300

    secret key

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mwaiyusuf@localhost/pitcher'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mwai@localhost/moha'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
     #  email configurations
-    MAIL_SERVER = 'mwaigalo5@gmail.com'
-    MAIL_PORT = 786
+    MAIL_SERVER = 'smtp.googlemail.com'  #enables  a transport layer securty to secure emails
+    MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
@@ -33,7 +33,7 @@ class TestConfig(Config):
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mwaiyusuf@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mwai@localhost/pitch'
     DEBUG = True
 
 config_options = {
