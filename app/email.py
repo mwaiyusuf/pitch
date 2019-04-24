5d1c3247b2d7abcaaf6ee42fce723b83bf935d62
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-24 13:58:47 +0300

    sending emails

--- app/email.py
+++ app/email.py
@@ -3,8 +3,8 @@ from flask import render_template
 from . import mail
 
 
-def mail_message(subject,template,to,**kwargs):
-    sender_email = "nstcephas@gmail.com"
+def mail_message(subject,template,to,**kwargs):  #taking four parameters
+    sender_email = "mwaigalo5@gmail.com"
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
